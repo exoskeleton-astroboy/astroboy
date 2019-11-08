116fd159e07b602a3106c99b703c0392da9e8c65
Big Mogician <sunaoran@youzan.com>
2019-11-08 12:16:11 +0800

    fix: publish 0.1.0

--- core/modules/service/call.ts
+++ core/modules/service/call.ts
@@ -1,6 +1,5 @@
 import { AstroboyContext, Bundles } from "@exoskeleton/core";
 import { Constructor } from "@bonbons/di";
-import { DubboConfig, IAjaxOptions, AjaxConfig } from "@core/typings/service";
 import { Service } from "astroboy";
 
 /**
@@ -10,48 +9,7 @@ import { Service } from "astroboy";
  * @export
  * @interface CallService
  */
-export interface CallService {
-  /**
-   * 集成 trace 调用 dubbo 接口
-   * @description
-   * @author Big Mogician
-   * @template T
-   * @param {string} serviceName
-   * @param {string} methodName
-   * @param {any[]} args
-   * @param {DubboConfig} [config]
-   * @returns {Promise<T>}
-   * @memberof CallService
-   */
-  dubboCall<T = any>(
-    serviceName: string,
-    methodName: string,
-    args: any[],
-    config?: DubboConfig
-  ): Promise<T>;
-  /**
-   * 通用 ajax 接口调用, 集成 trace && sc
-   * @description
-   * @author Big Mogician
-   * @template T
-   * @param {IAjaxOptions} ajaxOptions
-   * @param {AjaxConfig} config
-   * @returns {Promise<T>}
-   * @memberof CallService
-   */
-  httpCall<T = any>(ajaxOptions: IAjaxOptions, config: AjaxConfig): Promise<T>;
-  /**
-   * 通过 proxy 调用 http 接口. 对应 config 字段为 "PROXY_URL"
-   * @description
-   * @author Big Mogician
-   * @template T
-   * @param {IAjaxOptions} ajaxOptions
-   * @param {AjaxConfig} config
-   * @returns {Promise<T>}
-   * @memberof CallService
-   */
-  proxyCall<T = any>(ajaxOptions: IAjaxOptions, config: AjaxConfig): Promise<T>;
-}
+export interface CallService extends Service {}
 
 export const CallService: Constructor<
   CallService
